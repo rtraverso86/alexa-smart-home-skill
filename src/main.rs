cf597c3a86afc3d2b94a2e7767afd45fc508e74a
Riccardo Traverso <rtraverso86@gmail.com>
2024-04-20 01:47:46 +0200

    Translation draft complete

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-use std::env;
-use lambda_runtime::{service_fn, tracing::{self, Level}, Error, LambdaEvent};
-use serde_json::{json, Value};
+use std::{env, time::Duration};
+use reqwest;
+use lambda_http::{service_fn, Error, lambda_runtime::{self, LambdaEvent}, tracing::{self, Level}};
+use serde::Serialize;
+use serde_json::Value;
 
+#[derive(Serialize)]
+struct ResponseData {
+    event: EventData,
+}
 
-async fn handler(event: LambdaEvent<Value>) -> Result<Value, String> {
+#[derive(Serialize)]
+struct EventData {
+    payload: PayloadData,
+}
+
+#[derive(Serialize)]
+struct PayloadData {
+    #[serde(rename = "type")]
+    t: String,
+    message: String,
+}
+
+async fn handler(event: LambdaEvent<Value>) -> Result<String, String> {
     if tracing::enabled!(Level::DEBUG) {
         let evt = serde_json::to_string_pretty(&event.payload).or(Err("Could not serialize event.payload"))?;
         tracing::debug!("Event: {}", evt);
     }
 
-    let base_url = env::var("BASE_URL");
-    if let Err(e) = base_url {
-        tracing::error!("Please set BASE_URL environment variable");
-        return Err(format!("{}: please set BASE_URL environment variable", e.to_string()));
-    }
-    let base_url = base_url.unwrap().trim_end_matches('/');
+    let base_url = env::var("BASE_URL")
+        .map_err(|e| format!("{}: please set the BASE_URL environment variable", e))?;
+    let base_url = base_url.trim_end_matches('/');
 
     let directive = &event.payload["directive"];
     if directive.is_null() {
@@ -32,21 +47,62 @@ async fn handler(event: LambdaEvent<Value>) -> Result<Value, String> {
     if scope.is_null() {
         scope = &directive["payload"]["scope"];
     }
-    if (scope.is_null()) {
+    if scope.is_null() {
         return Err("Malformed request - missing endpoint.scope".into());
     }
     if scope["type"].to_string() != "BearerToken" {
         return Err("Malformed request - endpoint.scope.type only supports BearerToken".into());
     }
 
-    let token = &scope["token"];
-    if token.is_none() {
-        token = env::var("LONG_LIVED_ACCESS_TOKEN").unwr;
+    let token = &scope["token"].as_str();
+    let token = if token.is_none() && tracing::enabled!(Level::DEBUG) {
+        env::var("LONG_LIVED_ACCESS_TOKEN").unwrap()
+    } else {
+        token.unwrap().into()
+    };
+
+    let disable_ssl_verification = if let Ok(v) = env::var("NOT_VERIFY_SSL") {
+        v.parse().unwrap_or(false)
+    } else {
+        false
+    };
+
+    let client = reqwest::ClientBuilder::new()
+        .connect_timeout(Duration::from_secs(2))
+        .read_timeout(Duration::from_secs(10))
+        .danger_accept_invalid_certs(disable_ssl_verification)
+        .build()
+        .map_err(|e| format!("Could not build reqwest client: {}", e))?;
+    let response = client.post(format!("{}/api/alexa/smart_home", base_url))
+        .header("Authorization", format!("Bearer {}", token))
+        .header("Content-Type", "application/json")
+        .body(serde_json::to_string(&event.payload).or(Err("Could not serialize response body"))?)
+        .send()
+        .await
+        .map_err(|e| format!("An error occurred while awaiting the http response: {}", e))?;
+    let response_status = response.status();
+    let response_data = response.text()
+        .await
+        .map_err(|e| format!("Could not extract response data: {}", e))?;
+
+    if !response_status.is_success() {
+        let val = ResponseData {
+            event: EventData {
+                payload: PayloadData {
+                    t: (if [401, 403].contains(&response_status.as_u16()) {
+                            "INVALID_AUTHORIZATION_CREDENTIAL"
+                        } else {
+                            "INTERNAL_ERROR"
+                        }).to_owned(),
+                    message: response_data,
+                }
+            }
+        };
+        return Ok(serde_json::to_string(&val)
+            .map_err(|e| format!("Could not serialize ResponseData with error info: {}", e))?);
     }
 
-    Ok(Value::Null)
-    //let first_name = payload["firstName"].as_str().unwrap_or("world");
-    //Ok(json!({ "message": format!("Hello, {first_name}!") }))
+    Ok(response_data)
 }
 
 #[tokio::main]
